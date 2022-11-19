18377330cd89692aa9e0a8fcd2fb1997d4328d43
Brian Stern <sterno2510@gmail.com>
2022-11-19 10:52:42 -0500

    removed uneeded code

--- src/App.jsx
+++ src/App.jsx
@@ -4,17 +4,11 @@ import Landing from './Landing.jsx';
 import { motion } from "framer-motion";
 import { Icon } from '@iconify/react';
 
-const Component = React.forwardRef((props, ref) => (
-  <Icon icon="mdi:bird" color="#d9f0ff" width="100" height="100"ref={ref} />
-))
-const MovingBird = motion(Component);
-
 export default function App() {
 
   return (
     <>
      <motion.div style={{alignItems:'left'}}
-      initial={{x:50}}
       animate={{ x: "calc(100vw - 50%)" }}
       transition={{ duration: 20 }}>
         <Icon icon="mdi:bird" color="#d9f0ff" width="100" height="100"/>
