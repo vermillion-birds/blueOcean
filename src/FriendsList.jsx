c96bb4d948667803b845b26fb9cc436ade725f03
Andy Simmons <rocknswimmer116@gmail.com>
2022-11-21 17:45:32 -0500

    added new bird form, some functionality, and linter ignores

--- src/FriendsList.jsx
+++ src/FriendsList.jsx
@@ -1,3 +1,5 @@
+/* eslint-disable arrow-body-style */
+/* eslint-disable react/function-component-definition */
 import React, {useState, useEffect} from 'react';
 import FriendEntry from './FriendEntry.jsx';
 
@@ -6,12 +8,12 @@ const FriendsList = (props) => {
 
   const onFriendSearch = (e) => {
     setFriendSearch(e.target.value);
-  }
+  };
 
   useEffect(() => {
     if (friendSearch.length !== 0) {
       console.log(friendSearch);
-      //sort all users where username or birds sceen name matches term
+      // sort all users where username or birds sceen name matches term
     } else {
       console.log('return to seeing all friends')
     }
@@ -22,12 +24,12 @@ const FriendsList = (props) => {
       <input type="text" placeholder="Find Fellow Birders" onChange={onFriendSearch} />
       <h1>Your Friends</h1>
       {[1,1,1].map((bird, i) => {
-        return <FriendEntry key={i} />
+        return (<FriendEntry key={i} />);
       })}
 
     </div>
-  )
-}
+  );
+};
 
 export default FriendsList;
 
