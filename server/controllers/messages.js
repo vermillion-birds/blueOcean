04db4d0d49d734621712d3fc990c9e7eff028c95
Lu Dong <odong0813@gmail.com>
2022-11-24 01:03:40 -0500

    getMessages returns empty array if no messages

--- server/controllers/messages.js
+++ server/controllers/messages.js
@@ -4,7 +4,13 @@ const getMessages = (req, res) => {
   console.log(req.params.chatIdString);
   getAllMessages(req.params.chatIdString)
   .then(data => {
-    res.send(data.rows[0].chats);
+    console.log(data.rows);
+    if (data.rows[0]){
+      res.send(data.rows[0].chats);
+    } else {
+      res.send([]);
+    }
+
   })
   .catch(err => console.log('ERROR IN GETMESSAGES ', err))
 
