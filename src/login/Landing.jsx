4a10ba1c4c0859b366139d9b6e226daedc42cf85
Brian Stern <sterno2510@gmail.com>
2022-11-23 21:35:15 -0500

    commiting birds chirping

--- src/login/Landing.jsx
+++ src/login/Landing.jsx
@@ -9,6 +9,7 @@ import { useHistory } from 'react-router-dom';
 import { motion } from 'framer-motion';
 import AccountPage from './AccountPage.jsx';
 import UserSignUp from './UserSignUp.jsx';
+import chirp from '../assets/birds-chirping-04-6771.mp3';
 
 const Container = styled.div`
   display: grid;
@@ -40,6 +41,12 @@ const userdb = {
 const Landing = ({ setGlobalUser, globalUser }) => {
   const [addUserToggle, setAddUserToggle] = useState(false);
   const history = useHistory();
+  const audio = new Audio(chirp);
+  audio.loop = true;
+
+  const play = () => {
+    audio.play();
+  };
 
   const {
     user,
@@ -89,7 +96,7 @@ const Landing = ({ setGlobalUser, globalUser }) => {
           <h1>Birder</h1>
           <motion.div
             style={{ display: 'flex' }}
-            animate={{ x: [-140, 155, -140], y: [0, 50, -170, -190, 0], rotateY: [0, 180, 0] }}
+            animate={{ x: [-140, 155, -140], y: [0, 20, -170, -170, 0], rotateY: [0, 180, 0] }}
             transition={{ duration: 7, repeat: 'Infinity' }}
             whileHover={{
               scale: 2,
@@ -97,18 +104,11 @@ const Landing = ({ setGlobalUser, globalUser }) => {
             onHoverStart={(e) => {}}
             onHoverEnd={(e) => {}}
           >
-            {/* <FlyingBird /> */}
             <Icon icon="mdi:bird" color="#d9f0ff" width="100" height="100" />
           </motion.div>
         </div>
-        {/* <div>
-          <Icon icon="mdi:bird" color="#d9f0ff" width="100" height="100" />
-        </div> */}
         <LandingButton onClick={() => login()}>Login</LandingButton>
-        {/* <div>Don't Have An Account?</div> */}
-        {/* <LandingButton onClick={() => { addUser(); }}>Create Account</LandingButton> */}
-        {/* {addUserToggle
-        && <UserSignUp setAddUserToggle={setAddUserToggle} />} */}
+        <button type="button" onClick={() => play()}>Full User Experience</button>
       </Container>
       )}
       {isAuthenticated
