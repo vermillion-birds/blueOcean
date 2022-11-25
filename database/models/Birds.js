4477037b53274d5ebc4ee1ec47050f703ebd7ff3
Andy Simmons <rocknswimmer116@gmail.com>
2022-11-25 16:13:58 -0500

    getting notes from data base now.

--- database/models/Birds.js
+++ database/models/Birds.js
@@ -32,6 +32,7 @@ const getAllBirdCardInfo = (user_id) => {
       'summary', birds.summary,
       'first_seen', bird_user.first_seen,
       'last_seen', bird_user.last_seen,
+      'note', bird_user.note,
       'count', bird_user.count,
       'bird_photos', (SELECT json_agg(bird_photos.photo_url) FROM bird_photos WHERE bird_photos.bird_id = birds.bird_id),
       'bird_location', (SELECT json_agg(
