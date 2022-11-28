ce2a93c0656ec35f1bdda34c3bab9a4fad95fd51
Andy Simmons <rocknswimmer116@gmail.com>
2022-11-27 22:05:36 -0500

    discover

---
+++ src/Discover.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './assets/FriendList.css';
+
+const Discover = ({allBirds}) => {
+  return (
+    <div className="discover">
+      discover here
+    </div>
+  )
+}
+
+export default Discover;
